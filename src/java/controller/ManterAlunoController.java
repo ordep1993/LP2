b2fcb4d930d60b72bebab7a9e5bf976a73d3f0e3
Aluno <Aluno@10.2.3.126>
2016-10-13 10:26:06 -0300

    alô

--- src/java/controller/ManterAlunoController.java
+++ src/java/controller/ManterAlunoController.java
@@ -7,10 +7,12 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modelo.Aluno;
 
 /**
  *
@@ -27,23 +29,35 @@ public class ManterAlunoController extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ManterAlunoController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ManterAlunoController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String acao = request.getParameter("acao");
+        if (acao.equals("prepararIncluir")) {
+            prepararIncluir(request, response);
+        } else if (acao.equals("confirmarIncluir")) {
+            //confirmarIncluir(request , response);
+        } else if (acao.equals("prepararEditar")) {
+            //prepararEditar(request , response);
+        } else if (acao.equals("confirmarEditar")) {
+            //confirmarEditar(request , response);
+        } else if (acao.equals("prepararExcluir")) {
+            //prepararExcluir(request , response);
+        } else if (acao.equals("confirmarExcluir")) {
+            //confirmarExcluir(request , response);
         }
     }
 
+    private void prepararIncluir(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            request.setAttribute("operacao", "Incluir");
+            request.setAttribute("alunos", Aluno.obterAluno());
+            RequestDispatcher view = request.getRequestDispatcher("/manterAluno.jsp");
+            view.forward(request, response);
+        } catch (ServletException ex) {
+        } catch (IOException ex) {
+        } catch (ClassNotFoundException ex) {
+        }
+    }
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
